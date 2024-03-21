bd3a15622d1220cf903383fe60b6a87224d41ec1
Yangyang <2191746730@qq.com>
2024-03-21 21:32:10 +0800

    fix async

--- src/main/kotlin/com/gitee/planners/api/common/task/SimpleFutureTask.kt
+++ src/main/kotlin/com/gitee/planners/api/common/task/SimpleFutureTask.kt
@@ -5,7 +5,7 @@ import taboolib.common.platform.function.submitAsync
 
 open class SimpleFutureTask(val tick: Long, async: Boolean = false, val onClose: Runnable) {
 
-    private val task = submit(async = true, delay = tick) {
+    private val task = submit(async = async, delay = tick) {
         this@SimpleFutureTask.handleClose()
     }
 
