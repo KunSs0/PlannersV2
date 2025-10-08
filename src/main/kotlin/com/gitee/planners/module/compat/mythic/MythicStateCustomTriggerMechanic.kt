db868e137dab5b29cfcd3f00fbed4b1fbb2d6232
KunSs <2191746730@qq.com>
2025-10-08 17:15:13 +0800

    update

---
+++ src/main/kotlin/com/gitee/planners/module/compat/mythic/MythicStateCustomTriggerMechanic.kt
@@ -0,0 +1,34 @@
+﻿package com.gitee.planners.module.compat.mythic
+
+import com.gitee.planners.api.job.target.adaptTarget
+import com.gitee.planners.core.skill.entity.state.States
+import io.lumine.xikage.mythicmobs.adapters.AbstractEntity
+import io.lumine.xikage.mythicmobs.io.MythicLineConfig
+import io.lumine.xikage.mythicmobs.skills.ITargetedEntitySkill
+import io.lumine.xikage.mythicmobs.skills.SkillMechanic
+import io.lumine.xikage.mythicmobs.skills.SkillMetadata
+import io.lumine.xikage.mythicmobs.skills.placeholders.parsers.PlaceholderString
+import taboolib.common.platform.Ghost
+import taboolib.common.platform.function.warning
+
+@Ghost
+class MythicStateCustomTriggerMechanic(config: MythicLineConfig) : SkillMechanic(config.line, config), ITargetedEntitySkill {
+
+    private val triggerName: PlaceholderString = config.getPlaceholderString(arrayOf("name", "trigger", "id"), "")
+
+    override fun castAtEntity(data: SkillMetadata, target: AbstractEntity?): Boolean {
+        if (target == null || !target.isLiving) {
+            return false
+        }
+
+        val name = triggerName.get(data, target).trim()
+        if (name.isEmpty()) {
+            warning("MythicStateCustomTriggerMechanic: name parameter is empty.")
+            return false
+        }
+
+        val targetEntity = target.bukkitEntity?.adaptTarget() ?: return false
+        States.trigger(targetEntity, name)
+        return true
+    }
+}
