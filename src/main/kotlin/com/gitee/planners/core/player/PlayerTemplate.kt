4a1713d07a39faff5c3c6a07affb94e48d0eb273
KunSs <2191746730@qq.com>
2026-06-23 00:16:03 +0800

    修复玩家路由初始化

--- src/main/kotlin/com/gitee/planners/core/player/PlayerTemplate.kt
+++ src/main/kotlin/com/gitee/planners/core/player/PlayerTemplate.kt
@@ -10,10 +10,10 @@ import org.bukkit.entity.Player
 import taboolib.common.platform.function.submitAsync
 import java.util.concurrent.CompletableFuture
 
-class PlayerTemplate(val id: Long, val onlinePlayer: Player, route: PlayerRoute?, map: Map<String, Metadata>) :
+class PlayerTemplate(val id: Long, val onlinePlayer: Player, initialRoute: PlayerRoute?, map: Map<String, Metadata>) :
     MetadataContainer(map), Leveling {
 
-    var route = route
+    var route = initialRoute
         set(value) {
             if (value == null && field != null) {
                 val skills = field!!.getImmutableSkillValues().mapNotNull { field!!.getSkillOrNull(it) }
@@ -24,18 +24,7 @@ class PlayerTemplate(val id: Long, val onlinePlayer: Player, route: PlayerRoute?
                 }
             }
             field = value
-            if (value != null) {
-                val routerId = value.routerId
-                if (playerRouter == null || playerRouter!!.routerId != routerId) {
-                    playerRouter = Database.INSTANCE.loadPlayerRouter(id, routerId)
-                    if (playerRouter == null) {
-                        val initialLevel = value.router.algorithmLevel?.minLevel
-                            ?: AlgorithmLevel.default?.minLevel
-                            ?: 1
-                        playerRouter = Database.INSTANCE.createPlayerRouter(id, routerId, initialLevel)
-                    }
-                }
-            }
+            refreshPlayerRouterByRoute(value)
             submitAsync {
                 Database.INSTANCE.updateRoute(this@PlayerTemplate)
             }
@@ -43,6 +32,44 @@ class PlayerTemplate(val id: Long, val onlinePlayer: Player, route: PlayerRoute?
 
     var playerRouter: PlayerRouter? = null
         private set
+
+    init {
+        refreshPlayerRouterByRoute(route)
+    }
+
+    /**
+     * 根据当前路线反查或创建玩家的路由等级数据。
+     *
+     * @param route 当前路线。
+     */
+    private fun refreshPlayerRouterByRoute(route: PlayerRoute?) {
+        if (route == null) {
+            playerRouter = null
+            return
+        }
+
+        val routerId = route.routerId
+        val currentPlayerRouter = playerRouter
+        if (currentPlayerRouter != null && currentPlayerRouter.routerId == routerId) {
+            return
+        }
+
+        var loadedPlayerRouter = Database.INSTANCE.loadPlayerRouter(id, routerId)
+        if (loadedPlayerRouter == null) {
+            var initialLevel = 1
+            val algorithmLevel = route.router.algorithmLevel
+            if (algorithmLevel != null) {
+                initialLevel = algorithmLevel.minLevel
+            } else {
+                val defaultAlgorithmLevel = AlgorithmLevel.default
+                if (defaultAlgorithmLevel != null) {
+                    initialLevel = defaultAlgorithmLevel.minLevel
+                }
+            }
+            loadedPlayerRouter = Database.INSTANCE.createPlayerRouter(id, routerId, initialLevel)
+        }
+        playerRouter = loadedPlayerRouter
+    }
 
     val level: Int
         @JvmName("level0")
